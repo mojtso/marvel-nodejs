36533b737418089d39f9b30ae9e3518dc7af9dda
User <user@Users-MacBook-Pro.local>
2018-11-29 23:49:12 +0200

    ls

--- app/constrollers/index.js
+++ app/constrollers/index.js
@@ -1,7 +1,55 @@
+let request = require('request');
+let md5 = require('md5');
+
 exports.get_characters = (req, res) => {
 
-    let character = {
-        id: 1011297,
+    const marvel_url = "https://gateway.marvel.com/v1/public/characters";
+    const privateKey = "f73672ce2f8f9b88e855722dee6461316678b4b4";
+
+    // Set the headers
+    var headers = {
+        'Content-Type':     'application/x-www-form-urlencoded',
+    }
+
+    // Configure the request
+    let date = new Date();
+    console.log(date+'');
+
+    var options = {
+        url: marvel_url,
+        method: 'GET',
+        headers: headers,
+        qs: {
+            'apikey': '60a07f6996f6157b689898fc65f03be9', 
+            'ts': date+'',
+            'hash': md5(privateKey)
+        }
+    }
+
+    // Start the request
+    request(options, function (error, response, body) {
+        if (error) {
+            console.log(error);
+        }
+
+        if(response.statusCode >= 200 && response.statusCode <= 500) {
+            console.log(body)
+        }
+    });
+
+    
+
+    res.status(200).json({ });
+};
+
+/*
+
+var characters = [];
+    characters.push(character_1);
+    characters.push(character_2);
+
+let character_1 = {
+        id: 1,
         name: "Agent Brand",
         description: "Test description",
         modified: "2013-10-24T13:09:30-0400",
@@ -11,8 +59,15 @@ exports.get_characters = (req, res) => {
         }
     }
 
-    var characters = [];
-    characters.push(character);
+    let character_2 = {
+        id: 2,
+        name: "Test Lastname",
+        description: "Test Also",
+        modified: "2013-10-24T13:09:30-0400",
+        thumbnail: {
+            path: "http://i.annihil.us/u/prod/marvel/i/mg/4/60/52695285d6e7e",
+            extension: "jpg"
+        }
+    }
 
-    res.status(200).json({ characters });
-};
+ */
