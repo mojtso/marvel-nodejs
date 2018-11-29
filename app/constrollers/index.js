69d111cc734b667d9a7afb957e0521517c85fee5
User <user@Users-MacBook-Pro.local>
2018-11-30 01:00:12 +0200

    parse results

--- app/constrollers/index.js
+++ app/constrollers/index.js
@@ -36,7 +36,7 @@ exports.get_characters = (req, res) => {
         if(response.statusCode >= 200 && response.statusCode <= 500) {
             var characters = [];
             characters = body.results;
-            console.log(body.results)
+            console.log(JSON.parse(body.results));
             res.status(200).json(characters);
         }
     });
