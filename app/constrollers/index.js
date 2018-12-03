67bce4241a8b48c582dcdf7409704fdf5270c470
Mojalefa <cmtsoaeli@gmail.com>
2018-12-03 15:29:26 +0200

    add comment

--- app/constrollers/index.js
+++ app/constrollers/index.js
@@ -5,7 +5,7 @@ exports.get_characters = (req, res) => {
 
     const marvel_url = "https://gateway.marvel.com/v1/public/characters";
     const apiKey = "60a07f6996f6157b689898fc65f03be9"
-    const privateKey = process.env.PRIVATE_KEY;
+    const privateKey = process.env.PRIVATE_KEY; //get key on server
 
     // Set the headers
     var headers = {
@@ -27,21 +27,15 @@ exports.get_characters = (req, res) => {
         }
     }
 
-    
-
     // Start the request
     request(options, function (error, response, body) {
         if (error) {
             res.statusCode(400).json({message: error});
-        }
-
-        
+        } 
 
         if(response.statusCode >= 200 && response.statusCode <= 500) {
             var newJson = JSON.parse(body);
             var characters = [];
-
-            console.log(newJson.data);
 
             if(newJson.data && newJson.data.results.length > 0) {
                 newJson.data.results.forEach(element => {
